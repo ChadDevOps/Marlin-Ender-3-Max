efb98baa5ece61dadd98b1529cd6b67ccd8e90dc
Chad Schwieterman <chad.devops@gmail.com>
2021-03-21 14:59:15 -0400

    Updated author

--- Configuration.h
+++ Configuration.h
@@ -69,7 +69,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Dust, valerionew, Ender-3)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Chad, Ender-3 Max)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
